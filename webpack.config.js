5cd0ad1c1dd8955e11899151e15d4c50a4f6e64a
Leslie Khumalo <leslie.khumalo@icloud.com>
2016-10-06 15:34:34 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,17 +13,37 @@ var config = {
 	
    module: {
       loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-				
-            query: {
-               presets: ['es2015', 'react']
+          {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel',
+              query: {
+                  presets: ['es2015', 'react']
+                }
+            },
+            
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
             }
-         }
-      ]
-   }
+        ]
+    }
 }
 
 module.exports = config;
+
+
+
+
