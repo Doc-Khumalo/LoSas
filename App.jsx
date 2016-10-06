2b264d2566c8ab9997ea1cbc0b7308209e514849
Leslie Khumalo <leslie.khumalo@icloud.com>
2016-10-06 15:47:29 +0100

    update

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            LoSas!!!
+         </div>
+      );
+   }
+}
+
+export default App;
